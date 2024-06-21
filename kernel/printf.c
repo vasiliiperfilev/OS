6e58c46e238fa6f00af724a86c6fd8d8d693cd1f
Vasilii <vasily.perfilyev@gmail.com>
2024-06-21 11:29:28 -0700

    lab3 solution

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,13 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace() {
+  uint64 fp = r_fp();
+  uint64 stackend = PGROUNDUP(fp);
+  do {
+    printf("%p\n", *(uint64 *)(fp - 8));
+    fp = *(uint64 *)(fp - 16); 
+  } while (fp < stackend);
+}
